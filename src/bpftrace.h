7d31f30ad93a07078559c3a40f9d4dad68d14c2b
Alastair Robertson <ajor@meta.com>
2023-12-06 08:15:12 -0800

    start cleanup

--- src/bpftrace.h
+++ src/bpftrace.h
@@ -163,7 +163,7 @@ public:
   virtual bool is_traceable_func(const std::string &func_name) const;
   virtual std::unordered_set<std::string> get_func_modules(
       const std::string &func_name) const;
-  virtual const struct stat &get_pidns_self_stat() const;
+  virtual bool running_in_pid_ns() const;
 
   int create_pcaps(void);
   void close_pcaps(void);
