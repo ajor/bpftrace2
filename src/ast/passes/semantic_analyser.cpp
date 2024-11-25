0a3a97b8a273fd3c2a6eb92ab8b666f8c23aa0f6
Alastair Robertson <ajor@meta.com>
2024-11-25 09:27:34 -0800

    AST: Add AddrOf node for getting the address of an in-memory object

--- src/ast/passes/semantic_analyser.cpp
+++ src/ast/passes/semantic_analyser.cpp
@@ -1751,6 +1751,12 @@ void SemanticAnalyser::visit(Variable &var)
   var.type = CreateNone();
 }
 
+void SemanticAnalyser::visit(AddrOf &addrof)
+{
+  addrof.expr->accept(*this);
+  addrof.type = CreatePointer(addrof.expr->type, AddrSpace::bpf);
+}
+
 void SemanticAnalyser::visit(ArrayAccess &arr)
 {
   Visit(arr.expr);
